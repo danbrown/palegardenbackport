2bff454e0b25ab020f14b2cd9d07ae54640bfc37
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-11-22 18:30:10 -0300

    cleaned code

--- src/main/kotlin/com/dannbrown/deltaboxlib/common/DeltaboxLib.kt
+++ src/main/kotlin/com/dannbrown/deltaboxlib/common/DeltaboxLib.kt
@@ -3,7 +3,6 @@ package com.dannbrown.deltaboxlib.common
 import com.dannbrown.deltaboxlib.common.init.DeltaboxBlocks
 import com.dannbrown.deltaboxlib.common.init.DeltaboxLang
 import com.dannbrown.deltaboxlib.platform.registrate.DeltaboxRegistrate
-import com.dannbrown.deltaboxlib.platform.util.DeltaboxUtil
 import com.tterrag.registrate.providers.ProviderType
 import net.minecraft.data.DataGenerator
 import org.slf4j.LoggerFactory
@@ -14,21 +13,11 @@ object DeltaboxLib {
     val LOGGER = LoggerFactory.getLogger(MOD_ID)
     val REGISTRATE = DeltaboxRegistrate(MOD_ID)
 
-    fun id(path: String) = DeltaboxUtil.resourceLocation(MOD_ID, path)
-
-    private var initialized = false
-
     fun init() {
         DeltaboxBlocks.register()
     }
 
     fun gatherData(gen: DataGenerator.PackGenerator) {
         REGISTRATE.addDataGenerator(ProviderType.LANG, DeltaboxLang::addLang)
-    }
-
-    @JvmStatic
-    fun postInit() {
-        if (initialized) return
-        initialized = true
     }
 }
