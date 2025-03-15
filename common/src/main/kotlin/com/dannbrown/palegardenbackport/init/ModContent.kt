e3e53b7bcd25d56ac957e480cec9a3954886cf6e
Daniel Brown <danielbrown25.9.2@gmail.com>
2025-03-15 11:07:02 -0300

    added items

--- common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModContent.kt
+++ common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModContent.kt
@@ -3,13 +3,12 @@ package com.dannbrown.palegardenbackport.init
 import com.dannbrown.deltaboxlib.init.DeltaboxRegistrate
 import com.dannbrown.deltaboxlib.registrate.util.CreativeTabsUtil
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 
 object ModContent {
   const val MOD_ID = "palegardenbackport"
   var REGISTRATE = DeltaboxRegistrate(MOD_ID)
 
-  val TAB = REGISTRATE.creativeTab(MOD_ID, "Pale Garden Backport", { ItemStack(Items.DIAMOND) }, { p, o ->
+  val TAB = REGISTRATE.creativeTab(MOD_ID, "Pale Garden Backport", { ItemStack(ModItems.MOD_ICON.get()) }, { p, o ->
     CreativeTabsUtil.displayAll(
       REGISTRATE, p, o
     )
--- common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModItems.kt
+++ common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModItems.kt
@@ -1,6 +1,12 @@
 package com.dannbrown.palegardenbackport.init
 
+import com.dannbrown.deltaboxlib.init.DeltaboxTags
+import com.dannbrown.palegardenbackport.init.ModContent.REGISTRATE
+import net.minecraft.world.item.Item
+
 object ModItems {
+  val MOD_ICON = REGISTRATE.item<Item>("mod_icon").itemTags(DeltaboxTags.ITEM.EXCLUDE_FROM_CREATIVE).register()
+  val RESIN_BRICK = REGISTRATE.item<Item>("resin_brick").register()
 
   fun register() {
     // init
