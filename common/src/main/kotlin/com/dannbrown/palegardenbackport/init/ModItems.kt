34cf97a4090cd06444a290ad5e7851f00d5953e2
Daniel Brown <danielbrown25.9.2@gmail.com>
2025-03-17 23:12:29 -0300

    brick recipe

--- common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModItems.kt
+++ common/src/main/kotlin/com/dannbrown/palegardenbackport/init/ModItems.kt
@@ -2,13 +2,26 @@ package com.dannbrown.palegardenbackport.init
 
 import com.dannbrown.deltaboxlib.content.item.DeltaboxSpawnEggItem
 import com.dannbrown.deltaboxlib.init.DeltaboxTags
+import com.dannbrown.deltaboxlib.registrate.datagen.RegistrateRecipes
 import com.dannbrown.palegardenbackport.init.ModContent.REGISTRATE
+import net.minecraft.data.recipes.RecipeCategory
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.SpawnEggItem
+import net.minecraft.world.item.crafting.Ingredient
 
 object ModItems {
   val MOD_ICON = REGISTRATE.item<Item>("mod_icon").itemTags(DeltaboxTags.ITEM.EXCLUDE_FROM_CREATIVE).register()
-  val RESIN_BRICK = REGISTRATE.item<Item>("resin_brick").register()
+  val RESIN_BRICK = REGISTRATE.item<Item>("resin_brick")
+    .recipe { g, i ->
+      g.simpleCookingRecipe(
+        { i.get() },
+        { Ingredient.of(ModBlocks.RESIN_CLUMP.get()) },
+        RecipeCategory.DECORATIONS,
+        RegistrateRecipes.CookingRecipeType.SMELTING,
+        0.1f, 200
+      )
+    }
+    .register()
   val CREAKING_SPAWN_EGG =
     REGISTRATE.item<SpawnEggItem>("creaking_spawn_egg")
       .factory { p -> DeltaboxSpawnEggItem({ ModEntityTypes.CREAKING.get() }, -10526881, -231406, p) }
